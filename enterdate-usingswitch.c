2331539b24c1bb6f62f8004d0f471777efcda170
Sofia Gilardini <99358082+sofiagilardini@users.noreply.github.com>
2022-02-28 09:07:33 -0300

    Return valid dates only

---
+++ enterdate-usingswitch.c
@@ -0,0 +1,82 @@
+#define JAN 1
+#define FEB 2
+#define MAR 3
+#define APR 4
+#define MAY 5
+#define JUN 6
+#define JUL 7
+#define AUG 8 
+#define SEP 9
+#define OCT 10 
+#define NOV 11 
+#define DEC 12 
+
+
+#include <stdio.h>
+
+void printreturn(day,month) {
+    printf("The date chosen is %i/%i", day, month);
+}
+
+void errordate() {
+    printf("Invalid date entered");
+}
+
+int main() {
+
+int month; 
+int day; 
+
+// enter the date in format of day month, for example 25 12 (december 12th)
+printf("Enter date in format date month");
+scanf("%d %d,", &day, &month);
+
+
+switch (month) {
+
+    case JAN:
+    case MAR:
+    case MAY: 
+    case JUL:
+    case AUG:
+    case OCT:
+    case DEC:
+
+        if (day < 1 || day > 31) {
+            errordate() ;
+        }
+        else {
+        printreturn(day, month);
+        }
+        break;
+
+    case APR:
+    case JUN:
+    case SEP:
+    case NOV:
+
+        if (day < 1 || day > 30) {
+            errordate() ;
+        }
+        else {
+        printreturn(day, month) ; 
+        }
+        break;
+
+    case FEB: 
+        if (day < 1 || day > 28) {
+            errordate() ;
+        }
+        else {
+        printreturn(day, month) ;
+        }
+        break; 
+
+    default:
+        errordate();
+
+//end of switch
+}
+        
+//end of main()
+}
